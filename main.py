fa7c32ef4576589decb7887742503827929069f4
jabberwocky <amthomasiv@gmail.com>
2024-08-24 23:12:44 -0500

    added player controls

--- main.py
+++ main.py
@@ -3,22 +3,46 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
     #print("Starting asteroids!")
     #print(f"Screen width: {SCREEN_WIDTH}")
     #print(f"Screen height: {SCREEN_HEIGHT}")
 
+    #Player
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    updatable.add(player)
+    drawable.add(player)
+    
     while True:
         screen.fill((0,0,0))
+        #player.update(dt)
+        #player.draw(screen)
+
+        for upd in updatable:
+            upd.update(dt)
+
+        for drw in drawable:
+            drw.draw(screen)
+        
         pygame.display.flip()
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        dt = clock.tick(60) / 1000
+            
 if __name__ == "__main__":
     main()
 
